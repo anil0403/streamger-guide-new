8b78b49274ca301a5a3146d1bd1a5d231440b61f
anil0403 <anilshrestha0403.as@gmail.com>
2024-02-18 16:54:26 +0545

    suspense boundry added

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import React from "react";
+import { Suspense } from "react";
 
 interface LayoutProps {
   children?: React.ReactNode;
@@ -8,11 +9,13 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="flex flex-col gap-5 px-10 py-2">
-      <Navbar />
-      {children}
-      <Footer />
-    </div>
+    <Suspense>
+      <div className="flex flex-col gap-5 px-10 py-2">
+        <Navbar />
+        {children}
+        <Footer />
+      </div>
+    </Suspense>
   );
 };
 
