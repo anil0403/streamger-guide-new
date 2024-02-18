a8fdb302568e7ab468bd7633cea7176de3f621b7
anil0403 <anilshrestha0403.as@gmail.com>
2024-02-18 16:58:31 +0545

    suspense boundry added

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -9,13 +9,11 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <Suspense>
       <div className="flex flex-col gap-5 px-10 py-2">
         <Navbar />
         {children}
         <Footer />
       </div>
-    </Suspense>
   );
 };
 
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,19 +1,21 @@
 import FilterContent from "@/components/filter/filter";
 import { getContents } from "@/lib/action/content/action";
+import { Suspense } from "react";
 // import { useSession } from "next-auth/react";
 interface FilterContentProps {
   searchParams?: Record<string, string | string[]>;
 }
 
-export default async function Home({searchParams}: FilterContentProps) {
-
+export default async function Home({ searchParams }: FilterContentProps) {
   const genre = searchParams?.genre?.toString() || "";
   console.log("genre = ", genre);
   const contents = await getContents();
   console.log("contents from index = ", contents);
   return (
-    <div className="min-h-screen">
-      <FilterContent />
-    </div>
+    <Suspense>
+      <div className="min-h-screen">
+        <FilterContent />
+      </div>
+    </Suspense>
   );
 }
